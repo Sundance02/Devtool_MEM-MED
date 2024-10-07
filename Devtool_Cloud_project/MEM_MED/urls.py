e0e0dafa4e3fde862c2ffa8165dff502fe3a4393
65070153 Pitchayadit Wasinanon (นายพิชญดิฏฐ์ วศินานนท์) <pitchayaditwasinanon@gmail.com>
2024-10-07 22:10:56 +0700

    Push Header

--- Devtool_Cloud_project/MEM_MED/urls.py
+++ Devtool_Cloud_project/MEM_MED/urls.py
@@ -5,4 +5,5 @@ from MEM_MED import views
 urlpatterns = [
 
     path("medicine/", views.daily_medicine_detail.as_view(), name="show_course"),
+    path("medicine/add/", views.MedicineAddView.as_view(), name="add-medicine"),
 ]
