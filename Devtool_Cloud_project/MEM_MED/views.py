4e4368b4337dcbfd12928e8cd53ce311c37faa40
Kan <nattawat26907@gmail.com>
2024-10-08 00:22:19 +0700

    Edit views.py

--- Devtool_Cloud_project/MEM_MED/views.py
+++ Devtool_Cloud_project/MEM_MED/views.py
@@ -21,7 +21,7 @@ class calendar(View):
     
     def get(self, request, year=None, month=None):
         log = MedicationLog.objects.all()
-    
+
         if year is None or month is None:
             now = datetime.now()
             year = now.year
@@ -30,15 +30,12 @@ class calendar(View):
             year = int(year)
             month = int(month)
 
- 
         cal = cale.Calendar()
         month_days = cal.monthdayscalendar(year, month)
 
-       
         log_dates = {log.date_taken.day for log in log if log.date_taken.year == year and log.date_taken.month == month and log.missed == None}
         log_dates_missed = {log.date_taken.day for log in log if log.date_taken.year == year and log.date_taken.month == month and log.missed == True}
         log_dates_not_missed = {log.date_taken.day for log in log if log.date_taken.year == year and log.date_taken.month == month and log.missed == False}
-
         
         if month == 1:
             prev_month = 12
@@ -47,14 +44,13 @@ class calendar(View):
             prev_month = month - 1
             prev_year = year
 
-   
+
         if month == 12:
             next_month = 1
             next_year = year + 1
         else:
             next_month = month + 1
             next_year = year
-
 
         return render(request, 'calendar.html', {
             'month_days': month_days,
